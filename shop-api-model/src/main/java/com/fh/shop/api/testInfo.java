3bde52958844e3ce761bae14aae28b75a5357fd8
wangzq686 <18910206119@163.com>
2021-06-10 18:54:28 +0800

    testinfo1

---
+++ shop-api-model/src/main/java/com/fh/shop/api/testInfo.java
@@ -0,0 +1,12 @@
+package com.fh.shop.api;
+
+/**
+ * @author wzq
+ * @description
+ * @date 2021/6/10 18:50
+ */
+public class testInfo {
+    public static void main(String[] args) {
+        System.out.println("Git 分支");
+    }
+}
